225081fb7afa209023a7c14cf3795f6ab53f6305
EarthWormGym <adam56.beresford@gmail.com>
2023-10-09 17:57:30 +0100

    Added and implemented Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import LibraryPage from './Pages/LibraryPage';
 import LoginPage from './Pages/LoginPage';
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,19 +2,31 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
     return (
+        <>
         <nav className="nav">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/library">Library</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
+            <ul className="flex border-b">
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/library">Library</CustomLink>
+                <CustomLink to="/login">Login</CustomLink>
+            </ul>
+        </nav>
+      </>
+    )
+}
+
+interface CustomLinkProps {
+    to: string;
+    children: React.ReactNode;
+}
+
+function CustomLink({ to, children, ...props}: CustomLinkProps) {
+    const path = window.location.pathname
+    return(
+        <li className={path === to ? "active -mb-px mr-1" : "mr-1"}>
+            <Link className={path === to ? "active bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" : "bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"} to={to} {...props}>
+                {children}
+            </Link>
+        </li>
     )
 }
 
