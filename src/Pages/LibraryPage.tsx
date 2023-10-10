4a3574380c6de71a7c98e342848625bf9bbfdd38
EarthWormGym <adam56.beresford@gmail.com>
2023-10-10 17:00:57 +0100

    laying out form

--- src/Pages/LibraryPage.tsx
+++ src/Pages/LibraryPage.tsx
@@ -1,5 +1,4 @@
 import { ChangeEvent, useState } from 'react'
-import '../CSS/App.css'
 
 function LibraryPage() {
   const [bookName, setBookName] = useState("");
@@ -17,10 +16,29 @@ function LibraryPage() {
   return (
     <>
       <h1>Library</h1>
-      <div className="card">
-        <input type="text" onChange={handleChange} value={bookName}></input>
-        <button onClick={addBook}>Add book name</button>
-        <p>{books.join(', ')}</p>
+      <form>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <label className="form-label">Book title</label>
+            <input type="text" className="form-text-input" onChange={handleChange} value={bookName} placeholder="Shantaram" required/>
+          </div>
+          <div>
+            <label className="form-label">Author</label>
+            <input type="text" className="form-text-input" placeholder="Gregory David Roberts" required/>
+          </div>
+          <div>
+            <label className="form-label">Genre</label>
+            <input type="text" className="form-text-input" placeholder="Fiction" required/>
+          </div>
+          <div>
+            <label className="form-label">Picture</label>
+            <input type="text" className="form-text-input" placeholder="book.jpg" required/>
+          </div>
+        </div>
+        <button type="submit" className="submit-button">Add Book</button>
+      </form>
+      <div>
+        {books.join(', ')}
       </div>
     </>
   )
