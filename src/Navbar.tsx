5af276fe2db07c15d64a5e2be4bc3f08f2ceccb4
EarthWormGym <adam56.beresford@gmail.com>
2023-10-10 11:34:13 +0100

    Cleaner navigation and active css

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom"
+import React from 'react'
+import { Link, NavigateFunction, useNavigate } from 'react-router-dom'
 
 function Navbar() {
+    const navigate = useNavigate();
+
     return (
         <>
         <nav className="nav">
             <ul className="flex border-b">
-                <CustomLink to="/">Home</CustomLink>
-                <CustomLink to="/library">Library</CustomLink>
-                <CustomLink to="/login">Login</CustomLink>
+                <CustomLink to="/" navigate={navigate}>Home</CustomLink>
+                <CustomLink to="/library" navigate={navigate}>Library</CustomLink>
+                <CustomLink to="/login" navigate={navigate}>Login</CustomLink>
             </ul>
         </nav>
       </>
@@ -17,13 +20,15 @@ function Navbar() {
 interface CustomLinkProps {
     to: string;
     children: React.ReactNode;
+    navigate: NavigateFunction;
 }
 
-function CustomLink({ to, children, ...props}: CustomLinkProps) {
-    const path = window.location.pathname
+function CustomLink({ to, children, navigate}: CustomLinkProps) {
     return(
-        <li className={path === to ? "active -mb-px mr-1" : "mr-1"}>
-            <Link className={path === to ? "active bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" : "bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"} to={to} {...props}>
+        <li className={window.location.pathname === to ? "active -mb-px mr-1" : "mr-1"}>
+            <Link className={window.location.pathname === to ? "active bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold" : "bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"} 
+                to={to}
+                onClick={() => navigate(to)}>
                 {children}
             </Link>
         </li>
